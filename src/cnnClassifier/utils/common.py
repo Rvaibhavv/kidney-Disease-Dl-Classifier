7e8ec302b9df9f2f0741b07674b201ecedef87bc
Rvaibhavv <rvaibhav615@gmail.com>
2023-12-27 17:26:15 +0530

    commitments

--- src/cnnClassifier/utils/common.py
+++ src/cnnClassifier/utils/common.py
@@ -14,9 +14,9 @@ import base64
 def read_yaml(path_to_yaml: Path)-> ConfigBox:
     try:
         with open(path_to_yaml) as yaml_file:
-            content =yaml.safe_load(yaml_file)
-            logger.info(f"yaml file:{path_to_yaml} loaded successfully")
-            return ConfigBox
+            content = yaml.safe_load(yaml_file)
+            logger.info(f"yaml file: {path_to_yaml} loaded successfully")
+            return ConfigBox(content)
     except BoxValueError:
         raise ValueError("yaml file is empty")
     except Exception as e:
@@ -26,9 +26,9 @@ def read_yaml(path_to_yaml: Path)-> ConfigBox:
 @ensure_annotations
 def create_directories(path_to_directories: list, verbose=True):
     for path in path_to_directories:
-        os.makedirs(path_to_directories)
+        os.makedirs(path, exist_ok=True)
         if verbose:
-            logger.info(f"created directory at : {path}")
+            logger.info(f"created directory at: {path}")
 
 
 @ensure_annotations
