89e159c640615dde844101e1eaf5a288f298c956
Rvaibhavv <rvaibhav615@gmail.com>
2024-01-23 21:12:02 +0530

    training stage complete

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from cnnClassifier import logger
 from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+from cnnClassifier.pipeline.stage_03_model_training import ModelTrainingPipeLine
+
 
 STAGE_NAME = "Data ingestion stage"
 
@@ -24,3 +26,16 @@ try:
 except Exception as e:
         logger.exception(e)
         raise e
+
+
+STAGE_NAME ="TRAINING"
+
+try:
+        logger.info(f".....stage {STAGE_NAME} started ....")
+        obj=ModelTrainingPipeLine()
+        obj.main()
+        logger.info(f"..... stage {STAGE_NAME} completed .....")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
---
+++ src/cnnClassifier/pipeline/stage_03_model_training.py
@@ -0,0 +1,29 @@
+from cnnClassifier.config.configuration import ConfigurationManager
+from cnnClassifier.components.model_training import Training
+from cnnClassifier import logger
+
+STAGE_NAME ="TRAINING"
+
+class ModelTrainingPipeLine:
+    def __init__(self):
+        pass
+
+    def main(self):
+        config = ConfigurationManager()
+        training_config = config.get_training_config()
+        training = Training(config=training_config)
+        training.get_base_model()
+        training.train_valid_generator()
+        training.train()
+
+
+
+if __name__ =="__main__":
+    try:
+        logger.info(f".....stage {STAGE_NAME} started ....")
+        obj=ModelTrainingPipeLine()
+        obj.main()
+        logger.info(f"..... stage {STAGE_NAME} completed .....")
+    except Exception as e:
+        logger.exception(e)
+        raise e
