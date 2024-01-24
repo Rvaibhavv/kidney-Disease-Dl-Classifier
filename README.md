9636c2d5c87438eeee76c9a791418ca22c03a492
Rvaibhavv <rvaibhav615@gmail.com>
2024-01-24 22:27:39 +0530

    final

--- README.md
+++ README.md
@@ -8,3 +8,9 @@ export MLFLOW_TRACKING_URI=https://dagshub.com/Rvaibhavv/kidney-Disease-Dl-Class
 export MLFLOW_TRACKING_USERNAME=Rvaibhavv 
 
 export MLFLOW_TRACKING_PASSWORD=a37ad20e3c8a402cd86cae1238ec2ef1bed9ba0d 
+
+
+### following will work for conda enviroment
+conda env config vars set MLFLOW_TRACKING_URI=https://dagshub.com/Rvaibhavv/kidney-Disease-Dl-Classifier.mlflow --name env_name
+conda env config vars set MLFLOW_TRACKING_USERNAME=Rvaibhavv --name env_name
+conda env config vars set MLFLOW_TRACKING_PASSWORD=a37ad20e3c8a402cd86cae1238ec2ef1bed9ba0d --name env_name
--- src/cnnClassifier/pipeline/stage_04_model_evaluation.py
+++ src/cnnClassifier/pipeline/stage_04_model_evaluation.py
@@ -1,7 +1,7 @@
 from cnnClassifier.config.configuration import ConfigurationManager
 from cnnClassifier.components.model_evaluation_mlflow import Evaluation 
 from cnnClassifier import logger
-
+import os
 STAGE_NAME ="model evaluation"
 
 
@@ -10,12 +10,16 @@ class EvaluationPipeLine:
         pass
 
     def main(self):
+    #    os.environ["MLFLOW_TRACKING_URI"]="https://dagshub.com/Rvaibhavv/kidney-Disease-Dl-Classifier.mlflow"
+    #    os.environ["MLFLOW_TRACKING_USERNAME"]="Rvaibhavv"
+    #    os.environ["MLFLOW_TRACKING_PASSWORD"]="a37ad20e3c8a402cd86cae1238ec2ef1bed9ba0d"
        config = ConfigurationManager()
        eval_config =config.get_evaluation_config()
        evaluation= Evaluation(eval_config)
        evaluation.evaluation()
        evaluation.save_score()
        evaluation.log_into_mlflow()
+       
 
 
 if __name__ =="__main__":
